84b3b43d9fdc8457920c7db28a6bb4cf5b109393
HeesungB <hezo25@naver.com>
2023-04-10 16:44:31 +0900

    Add CheckItem Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { Styles } from "./styles";
 import { ItemType } from "./types";
 
-const tempItems: ItemType[] = [
-  { id: 1, label: "item 1" },
-  { id: 2, label: "item 2" },
-  { id: 3, label: "item 3" },
-  { id: 4, label: "item 4" },
-  { id: 5, label: "item 5" },
-];
-
 function App() {
+  const items: ItemType[] = [
+    { id: 1, label: "Block 1" },
+    { id: 2, label: "Block 2" },
+    { id: 3, label: "Block 3" },
+    { id: 4, label: "Block 4" },
+    { id: 5, label: "Block 5" },
+  ];
+
   return (
     <Styles.Container className="App">
       <Styles.Content>
@@ -19,10 +19,11 @@ function App() {
         </Styles.Top>
 
         <Styles.Title>
-          <input type="checkbox" />
+          <Styles.CheckBox type="checkbox" />
           <div>Select All</div>
 
           <Styles.Flex1 />
+
           <Styles.Buttons>
             <Styles.AddButton>Add</Styles.AddButton>
             <Styles.SortButton>Sort ASC</Styles.SortButton>
@@ -31,20 +32,29 @@ function App() {
         </Styles.Title>
         <Styles.Divider />
         <Styles.ItemList>
-          {tempItems.map((item) => {
-            return (
-              <Styles.Item key={item.id}>
-                <input type="checkbox" /> <div>{item.label}</div>
-                <Styles.Flex1 />
-                <Styles.Delete>X</Styles.Delete>
-              </Styles.Item>
-            );
+          {items.map((item) => {
+            return <CheckItem key={item.id} item={item} />;
           })}
         </Styles.ItemList>
       </Styles.Content>
     </Styles.Container>
   );
 }
+
+const CheckItem: FunctionComponent<{
+  item: ItemType;
+}> = ({ item }) => {
+  return (
+    <Styles.Item key={item.id}>
+      <Styles.CheckBox type="checkbox" readOnly />
+      <div>
+        <b>[{item.id}]</b> {item.label}
+      </div>
+      <Styles.Flex1 />
+      <Styles.Delete>X</Styles.Delete>
+    </Styles.Item>
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default App;
